7a67f5140243687789bc1d10a0c9fcd62628b5a5
marivan <ivan.maryshev.90@mail.ru>
2025-12-10 12:46:20 +0300

    token

--- bot.py
+++ bot.py
@@ -19,11 +19,7 @@ stop_words = stopwords.words('english')
 
 # Токен
 # Читаем токен из переменной окружения (безопасно!)
-TOKEN = os.getenv("BOT_TOKEN")
-
-if not TOKEN:
-    print("ОШИБКА: Не найден BOT_TOKEN! Добавь его в Environment Variables на Render.")
-    exit(1)
+TOKEN = "8275828988:AAEvoC1vldPuxBqy5As39J5Fo43YS0zScok"
 
 bot = telebot.TeleBot(TOKEN)
 
